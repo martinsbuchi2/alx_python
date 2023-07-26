fc299283f82b5e86886560509f34db6ffc14cf38
martinsibuchim <martinsibuchim@gmail.com>
2023-07-26 19:15:56 +0100

    last digit of a random number

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 
 # Get the last digit of the number
-last_digit = abs(number) % 10
+last_digit = number % 10 if number >= 0 else -(-number % 10)
 
 # Determine the output message based on the last digit
 if last_digit > 5:
