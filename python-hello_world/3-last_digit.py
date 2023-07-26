0084fb9959e59aa3d1f277fc91d3660c886e9773
martinsibuchim <martinsibuchim@gmail.com>
2023-07-26 17:05:07 +0100

    last digit of a randomly generated number

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+last_digit = abs(number) % 10
+# Determine the output message based on the last digit
+if last_digit > 5:
+    result = "and is greater than 5"
+elif last_digit == 0:
+    result = "and is 0"
+else:
+    result = "and is less than 6 and not 0"
+
+# Print the result
+print(f"The Last digit of {number} is {last_digit}, {result}")
+    
