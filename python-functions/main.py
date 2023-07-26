147cfea851e6a5c90beb543fd4574e1d62afcd0a
martinsibuchim <martinsibuchim@gmail.com>
2023-07-27 00:42:01 +0100

    test

--- python-functions/main.py
+++ python-functions/main.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 add = __import__('0-sum').add
+#print(add(1, 2))
+#print(add(98, 0))
+#print(add(100, -2))
 
-print(add(1, 2))
-print(add(98, 0))
-print(add(100, -2))
+pow = __import__('1-power').pow
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
