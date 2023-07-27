74d315d9ab10813949ef48792e9fc756a0038e01
martinsibuchim <martinsibuchim@gmail.com>
2023-07-27 14:29:39 +0100

    prime numbers

---
+++ python-functions/5-prime.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
