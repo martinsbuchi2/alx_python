d22a479551a098d7a651c510be310bb39b8d6470
martinsibuchim <martinsibuchim@gmail.com>
2023-07-29 23:03:25 +0100

    test

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -6,5 +6,6 @@ from add_0 import add
 
 result = add(a, b)
 
-if __name__ == "__main__":
-    print("{a} + {b} = {result}".format(a,b,result))
+#if __name__ == "__main__":
+    #print("{a} + {b} = {result}".format(a,b,result))
+print("{a} + {b} = {result}".format(a,b,result))
