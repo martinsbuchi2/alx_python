8ad33492c494f8dc2e7152daf76388d706278bca
martinsibuchim <martinsibuchim@gmail.com>
2023-07-29 22:58:02 +0100

    test

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,10 @@
 
-a =1
+a =1 
 b =2
 
 from add_0 import add
 
 result = add(a, b)
-output_string = f"{a} + {b} = {result}"
 
 if __name__ == "__main__":
-    print("{a} + {b} = {add(a, b)}".format(a,b,add(a, b)))
+    print("{a} + {b} = {result}".format(a,b,result))
