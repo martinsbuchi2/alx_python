88aa3c2eadf4f8525f4d866f2678582a8a783184
martinsibuchim <martinsibuchim@gmail.com>
2023-08-09 21:53:49 +0100

    Access and update private attribute

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""a class Square that defines a square"""
+    
+class Square:
+    
+    """instance attribute, then access and update private attribute"""
+    
+    def __init__(self, size):
+        self.size = size
+        
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+            
+   
+    def area(self):
+        return self.__size * self.__size
