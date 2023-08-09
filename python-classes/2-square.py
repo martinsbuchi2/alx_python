b13bdb51df7e46334c41d3d62cde70652a1e91c3
martinsibuchim <martinsibuchim@gmail.com>
2023-08-09 21:11:49 +0100

    get area of the square

---
+++ python-classes/2-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""a class Square that defines a square"""
+    
+class Square:
+    
+    """Private instance attribute"""
+ 
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+            
+    """public instance attribute"""
+    
+    def area(self):
+        return self.__size * self.__size
+    
+my_square_1 = Square(3)
+print("Area: {}".format(my_square_1.area()))
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+my_square_2 = Square(5)
+print("Area: {}".format(my_square_2.area()))
