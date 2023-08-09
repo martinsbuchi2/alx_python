30ca9bc3cead96063ee361e4fb2aee11e065850c
martinsibuchim <martinsibuchim@gmail.com>
2023-08-09 21:14:40 +0100

    get area of the square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -18,19 +18,3 @@ class Square:
     
     def area(self):
         return self.__size * self.__size
-    
-my_square_1 = Square(3)
-print("Area: {}".format(my_square_1.area()))
-
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-my_square_2 = Square(5)
-print("Area: {}".format(my_square_2.area()))
