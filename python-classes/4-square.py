dde6a4588d36e4fb53b0536eab09b55b41ce7c83
martinsibuchim <martinsibuchim@gmail.com>
2023-08-09 22:17:12 +0100

    Print a square

---
+++ python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""a class Square that defines a square"""
+    
+class Square:
+    
+    """instance attribute, then access and update private attribute"""
+    
+    def __init__(self, size):
+        self.__size = size
+        
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+            
+   
+    def area(self):
+        return self.__size * self.__size
+    
+    
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
