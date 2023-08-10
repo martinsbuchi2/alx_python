1121772d189d7e4c01865354ab3c04fb6f67021c
martinsibuchim <martinsibuchim@gmail.com>
2023-08-10 19:50:36 +0100

    Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
 """an empty class BaseGeometry"""
 
-class BaseGeometry:
-    pass
+class BaseGeometry():
     """an empty class BaseGeometry"""
+    pass
+
+class HideInitSubclass(type):
+    """hide init subclass"""
+    def __dir__(self):
+        attrs = super().__dir__()
+        return [attr for attr in attrs if attr != '__init_subclass__']
+
+class BaseGeometry(metaclass=HideInitSubclass):
+    """hide init subclass"""
+    pass
+
+bg = BaseGeometry()
+
+print(bg)
+print(dir(bg))
+print(dir(BaseGeometry))
