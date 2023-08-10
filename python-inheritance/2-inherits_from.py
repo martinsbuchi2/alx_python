ad46d7a97f5360441625fa0394ff1cc9ae1d66ad
martinsibuchim <martinsibuchim@gmail.com>
2023-08-10 16:11:24 +0100

    Only sub class of

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" My base class """
+class  isClass:
+    pass
+    
+def inherits_from(obj, a_class):
+    """ My base class """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
