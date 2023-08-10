c28af669dfda1889d150b7428d79d089a1da243d
martinsibuchim <martinsibuchim@gmail.com>
2023-08-10 22:06:00 +0100

    square

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""class"""
+
+class BaseGeometry:
+    """base class"""
+    
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """rectangle sub-class"""
+    
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+    
+    def area(self):
+        return self.__width * self.__height
+    
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+    
+    
+class Square(Rectangle):
+    """square sub-class"""
+    def __init__(self, size):
+        self.__size = 0
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+    
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
