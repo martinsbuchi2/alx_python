d15fe66c79a6793dc6d7dbbd5920f7daec049c57
martinsibuchim <martinsibuchim@gmail.com>
2023-08-10 20:32:11 +0100

    improve geometry

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -7,5 +7,7 @@ class BaseGeometry:
     """base class"""
     
     def area(self):
+        
         """public instance that reaise exception"""
+        
         raise Exception("area() is not implemented")
