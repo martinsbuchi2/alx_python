33e2f610264336087286336061c4c7a161879be3
martinsibuchim <martinsibuchim@gmail.com>
2023-08-10 21:59:31 +0100

    Full rectangle

---
+++ python-inheritance/7-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""class"""
+
+class BaseGeometry:
+    """base class"""
+    
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """inherit class sub-class"""
+    
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+    
+    def area(self):
+        return self.__width * self.__height
+    
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+    
