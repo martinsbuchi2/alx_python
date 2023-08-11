1c11520a4038b5a27204040520be0c207c8530e7
martinsibuchim <martinsibuchim@gmail.com>
2023-08-11 03:33:37 +0100

    improve geometry

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
-"""an empty class BaseGeometry"""
-
+"""BaseGeometry"""
 class BaseGeometry:
-    """an empty class BaseGeometry"""
+    """Base class; reomve __init_subclass__"""
+    def __dir__(cls) -> None:
     
-    pass
+        attributes = super().__dir__()
+        
+        list_to_return = []
+        
+        for attrs in attributes:
+            if attrs != '__init_subclass__':
+                list_to_return.append(attrs)
+        return list_to_return
+    
+    
+    #def __dir__(cls) -> None:
+    #    attributes = super().__dir__()
+        
+     #   return [attributes for attributes in attributes if attributes != '__init_subclass__']
