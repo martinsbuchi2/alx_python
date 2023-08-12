ec18fec153e1ff7b8151461a2b0bd8b254fb732c
martinsibuchim <martinsibuchim@gmail.com>
2023-08-12 13:04:31 +0100

    empty geometry

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
-"""BaseGeometry"""
-class BaseGeometry:
-    """Base class; reomve __init_subclass__"""
+"""
+write metaclass from which the BaseGeometry
+inherits.
+
+NB: All class inherites from type class by 
+default.
+"""
+class MyMetaClass(type):
+    """the metaclass that inherits from the the 
+       type class
+    """
     def __dir__(cls) -> None:
-    
+        """write a method that remove '__init_subclass__
+           from the dir() of the metaclass
+        """
         attributes = super().__dir__()
-        
-        list_to_return = []
-        
-        for attrs in attributes:
-            if attrs != '__init_subclass__':
-                list_to_return.append(attrs)
-        return list_to_return
+        return [attrs for attrs in attributes if attrs != '__init_subclass__']
     
+
+"""BaseGeometry"""
+class BaseGeometry(metaclass = MyMetaClass):
+    """subclass; BaseGeometry"""
+    def __dir__(cls) -> None:
+        """reomve __init_subclass__ from BaseGeometry"""
+        attributes = super().__dir__()
+        return [attrs for attrs in attributes if attrs != '__init_subclass__']
     
-    #def __dir__(cls) -> None:
-    #    attributes = super().__dir__()
-        
-     #   return [attributes for attributes in attributes if attributes != '__init_subclass__']
