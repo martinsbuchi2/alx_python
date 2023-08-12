f6e01970777e98950fe6439715fa11cffd0662da
martinsibuchim <martinsibuchim@gmail.com>
2023-08-12 14:14:11 +0100

    improve geometry

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
-"""class"""
+"""
+write metaclass from which the BaseGeometry
+inherits.
 
-class BaseGeometry:
-    """base class"""
+NB: All class inherites from type class by 
+default.
+"""
+class MyMetaClass(type):
+    """the metaclass that inherits from the the 
+       type class
+    """
+    def __dir__(cls) -> None:
+        """write a method that remove '__init_subclass__
+           from the dir() of the metaclass
+        """
+        attributes = super().__dir__()
+        return [attrs for attrs in attributes if attrs != '__init_subclass__']
+    
+
+"""BaseGeometry"""
+class BaseGeometry(metaclass = MyMetaClass):
+    """subclass; BaseGeometry"""
+    def __dir__(cls) -> None:
+        """reomve __init_subclass__ from BaseGeometry"""
+        attributes = super().__dir__()
+        return [attrs for attrs in attributes if attrs != '__init_subclass__']
     
     def area(self):
         raise Exception("area() is not implemented")
