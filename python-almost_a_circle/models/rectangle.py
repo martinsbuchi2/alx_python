1ca091ef80af61c01a256b5a3b3f7228ff529c23
martinsibuchim <martinsibuchim@gmail.com>
2023-08-17 03:10:29 +0100

    display #1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -73,8 +73,10 @@ class Rectangle(Base):
     
     def display(self):
         """Displays the Rectangle instance using '#' characters"""
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print('#' * self.__width)
+            print(" " * self.__x + '#' * self.__width)
             
     def __str__(self):
         """String representation of the Rectangle instance"""
