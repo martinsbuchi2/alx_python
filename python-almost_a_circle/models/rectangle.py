c901fd9814dc55cb43c9f25bf8bf3caa18cd89e1
martinsibuchim <martinsibuchim@gmail.com>
2023-08-17 01:12:51 +0100

    first rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,61 @@
+from base import Base
+
+class Rectangle(Base):
+    """Rectangle class, which inherit the Base class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor method for Rectangle class"""
+        super().__init__(id)  # Call Base class constructor with provided id
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        
+    @property
+    def width(self):
+        """Getter for width"""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        """Setter for width"""
+        self.__width = value
+        
+    @property
+    def height(self):
+        """Getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter for x"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter for y"""
+        self.__y = value
+        
+    
+r1 = Rectangle(10, 2)
+print(r1.id)
+
+r2 = Rectangle(2, 10) 
+print(r2.id)
+
+r3 = Rectangle(10, 2, 0, 0, 12)
+print(r3.id)
