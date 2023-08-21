f0865040ed00dff3678307b91a2d3005adf6492a
martinsibuchim <martinsibuchim@gmail.com>
2023-08-21 13:05:05 +0100

    Response header value

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,14 +2,7 @@
 import requests
 import sys
 
-import requests
-import sys
-
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py <URL>")
-        sys.exit(1)
-
     url = sys.argv[1]
     response = requests.get(url)
     
