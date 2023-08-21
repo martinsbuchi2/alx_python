ca313824bed0249c7e250f417877b51950e459b4
martinsibuchim <martinsibuchim@gmail.com>
2023-08-21 13:54:47 +0100

    post email

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -3,10 +3,6 @@ import requests
 import sys
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: python script_name.py <URL> <email>")
-        sys.exit(1)
-
     url = sys.argv[1]
     email = sys.argv[2]
 
