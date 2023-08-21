a990affba4c5c43c1a9dc147af0182fe35e42c7b
martinsibuchim <martinsibuchim@gmail.com>
2023-08-21 11:25:43 +0100

    response status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,4 +4,4 @@ import requests
 res = requests.get('https://alu-intranet.hbtn.io/status')
 print("Body response:")
 print("\t- type: {}".format(type(res.text)))
-print("\t- type: {}".format(res.text))
+print("\t- content: {}".format(res.text))
