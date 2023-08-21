7ae968eef7b9409f89baa9809a81bd1a2c05b3d5
martinsibuchim <martinsibuchim@gmail.com>
2023-08-21 14:34:57 +0100

    Error code

---
+++ python-network_1/4-error_code.py
@@ -0,0 +1,16 @@
+"""Python script that takes in a URL, sends a request to the URL and displays the body of the response."""
+import requests
+import sys
+
+def main():
+    url = url = sys.argv[1]
+    
+    res = requests.post(url)
+    if res.status_code >= 400:
+        print('Error code: {}'.format(res.status_code))
+    else:
+        print(res.text)    
+    
+
+if __name__ == '__main__':
+    main()
