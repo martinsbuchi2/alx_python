094db37dcd390d97cdd8ccce48d20f3bee9b552a
martinsibuchim <martinsibuchim@gmail.com>
2023-08-21 12:46:55 +0100

    header response

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,23 +2,22 @@
 import requests
 import sys
 
-import requests
-import sys
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python 1-hbtn_header.py <URL>")
+        return
 
-def get_x_request_id(url):
+    url = sys.argv[1]
     response = requests.get(url)
-    x_request_id = response.headers.get('X-Request-Id')
-    return x_request_id
+
+    if response.status_code == 200:
+        x_request_id = response.headers.get('X-Request-Id')
+        if x_request_id:
+            print(x_request_id)
+        else:
+            print("X-Request-Id not found in the response header.")
+    else:
+        print(f"Request to {url} failed with status code: {response.status_code}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py <URL>")
-        sys.exit(1)
-    
-    url = sys.argv[1]
-    x_request_id = get_x_request_id(url)
-    
-    if x_request_id:
-        print(x_request_id)
-    else:
-        print("X-Request-Id header not found in the response.")
+    main()
