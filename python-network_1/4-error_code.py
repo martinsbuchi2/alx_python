1ed68cfef98716c61a18550c29a9139a8555af40
martinsibuchim <martinsibuchim@gmail.com>
2023-08-21 14:39:55 +0100

    error code

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -3,13 +3,13 @@ import requests
 import sys
 
 def main():
-    url = url = sys.argv[1]
+    url = sys.argv[1]
+    response = requests.get(url)
     
-    res = requests.post(url)
-    if res.status_code >= 400:
-        print('Error code: {}'.format(res.status_code))
-     
-    
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
