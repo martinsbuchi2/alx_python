88c47b34e8d1e0b3a2f9a352d362dd665eb66e5f
martinsibuchim <martinsibuchim@gmail.com>
2023-08-25 13:02:42 +0100

    First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,15 +5,22 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
 Base = declarative_base()
+
 
 class State(Base):
     """State class inheriting from Base class """
     __tablename__ = 'states'
-    id = Column('id', Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column('id', Integer, primary_key=True, nullable=False,
+                autoincrement=True)
     name =  Column('name', String(128), nullable=False)
+
 
 if __name__ == "__main__":
     """ to be imported before calling """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], 
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
+
