b333068aa882835bb844c1675d84c00eadb7014a
martinsibuchim <martinsibuchim@gmail.com>
2023-08-25 12:48:59 +0100

    First state model

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+import sys
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column('id', Integer, primary_key=True, nullable=False, autoincrement=True)
+    name =  Column('name', String(128), nullable=False)
+    
+    
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
