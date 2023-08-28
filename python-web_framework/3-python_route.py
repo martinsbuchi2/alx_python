37beb0d8089512ea2d8191bdcab58990f650fbdc
martinsibuchim <martinsibuchim@gmail.com>
2023-08-28 15:20:54 +0100

    python is cool

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -9,6 +9,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
+""" doc"""
 @app.route("/", strict_slashes=False)  # route to the root of our site, "/"
 def hello():
     return "Hello HBNB!"
@@ -19,9 +20,11 @@ def hello_hbnb():
     return "HBNB"
 
 
+""" doc"""
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     return "C " + text.replace('_', ' ')
+
 
 """ doc"""
 @app.route('/python/', strict_slashes=False)
@@ -29,6 +32,7 @@ def c_text(text):
 def c_text(text):
     return  "Python " + text.replace('_', ' ')
 
+""" doc"""
 if __name__ == "__main__":
     """run the flask app"""
     app.run(host='0.0.0.0', port=5000)   # host is 'localhost'
