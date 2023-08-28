148a058c057ce657321661043bcc231e7f8750a0
martinsibuchim <martinsibuchim@gmail.com>
2023-08-28 15:12:32 +0100

    python is cool

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -23,6 +23,7 @@ def c_text(text):
     return "C " + text.replace('_', ' ')
 
 
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def c_text(text):
     return  "Python " + text.replace('_', ' ')
