5e5cf33fdd97a6643e9b9226aa1eb7570e8f6a34
martinsibuchim <martinsibuchim@gmail.com>
2023-08-28 13:39:35 +0100

    hello flask

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)  # route to the root of our site, "/"
 def hello():
-    return '“Hello HBNB!”'
+    return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)   # host is 'localhost'
