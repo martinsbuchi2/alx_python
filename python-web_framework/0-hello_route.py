ebbde699eb2c782787069663da2e5d12994e59b1
martinsibuchim <martinsibuchim@gmail.com>
2023-08-28 13:37:20 +0100

    Hello Flask!

---
+++ python-web_framework/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""
+ script that starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)  # route to the root of our site, "/"
+def hello():
+    return '“Hello HBNB!”'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)   # host is 'localhost'
