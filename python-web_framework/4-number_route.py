af64278f9670a049643ef16182fe8dfe274d06ca
martinsibuchim <martinsibuchim@gmail.com>
2023-08-28 23:36:42 +0100

    it is a number

---
+++ python-web_framework/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+"""
+the previous task to a new script that starts a Flask web application:
+/number/<n>: display “n is a number” only if n is an integer
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)  # route to the root of our site, "/"
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)  # route to the root of our site, "/"
+def hello_hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)   # host is 'localhost'
