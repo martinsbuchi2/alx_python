546bc19b1d14045b1e502d8288f40e9be3002273
martinsibuchim <martinsibuchim@gmail.com>
2023-08-28 20:50:53 +0100

    python is fun

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 """
-the previous task to a new script that 
-starts a Flask web application
+the previous task to a new script that starts a Flask web application
 """
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
-""" doc"""
 @app.route("/", strict_slashes=False)  # route to the root of our site, "/"
 def hello():
     return "Hello HBNB!"
@@ -19,19 +18,16 @@ def hello_hbnb():
     return "HBNB"
 
 
-""" doc"""
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     return "C " + text.replace('_', ' ')
 
 
-""" doc"""
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def c_text(text):
-    return  "Python " + text.replace('_', ' ')
+def python_text(text="is cool"):
+    return "Python " + text.replace('_', ' ')
 
-""" doc"""
+
 if __name__ == "__main__":
-    """run the flask app"""
     app.run(host='0.0.0.0', port=5000)   # host is 'localhost'
