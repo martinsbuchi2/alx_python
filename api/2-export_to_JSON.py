ff87f0fc85950939cdb67a78868fc479e5ef0251
martinsibuchim <martinsibuchim@gmail.com>
2023-10-11 16:07:49 +0100

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,6 +1,6 @@
 '''Using what you did in the task #0, extend your Python script to export data in the JSON format.'''
-import requests
 import json
+import requests
 import sys
 
 def fetch_employee_data(employee_id):
